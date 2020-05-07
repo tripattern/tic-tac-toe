779fb20951d6a27a8bb8f544cf8354eace9fa077
tripattern <systems@tripattern.com>
2020-05-07 13:41:04 +0200

    Updates

---
+++ precache-manifest.352c4899d2ec49a8bd25e8c1ae2d2e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874edff5c4487a10196954f0f5daeb53",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9642f82a8d12445be458",
+    "url": "/tic-tac-toe/static/css/main.8a935685.chunk.css"
+  },
+  {
+    "revision": "1670ec99ad6b858bae34",
+    "url": "/tic-tac-toe/static/js/2.56713c78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.56713c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9642f82a8d12445be458",
+    "url": "/tic-tac-toe/static/js/main.5eba5fb3.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
